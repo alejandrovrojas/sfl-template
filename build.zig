10e85ea9d844018853ae70e37701b44a147b6c83
Alejandro V. Rojas <6848068+alejandrovrojas@users.noreply.github.com>
2026-02-02 09:09:09 +0100

    Change compilation target

--- build.zig
+++ build.zig
@@ -4,17 +4,27 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "sfl-template",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .optimize = .ReleaseSmall,
-            .target = b.resolveTargetQuery(.{
-                .cpu_arch = .wasm32,
-                .os_tag   = .freestanding,
-            })
-        })
+            .root_source_file = b.path("template.main.zig"),
+            .target = b.graph.host,
+        }),
     });
 
-    exe.rdynamic = true;
-    exe.entry    = .disabled;
-
     b.installArtifact(exe);
+
+    // const exe = b.addExecutable(.{
+    //     .name = "sfl-template",
+    //     .root_module = b.createModule(.{
+    //         .root_source_file = b.path("src/main.zig"),
+    //         .optimize = .ReleaseSmall,
+    //         .target = b.resolveTargetQuery(.{
+    //             .cpu_arch = .wasm32,
+    //             .os_tag   = .freestanding,
+    //         })
+    //     })
+    // });
+    //
+    // exe.rdynamic = true;
+    // exe.entry    = .disabled;
+    //
+    // b.installArtifact(exe);
 }
