8cda36213d32fa1e8c58ffa4eb37e6bbc983e78d
Alejandro V. Rojas <6848068+alejandrovrojas@users.noreply.github.com>
2026-02-16 09:50:31 +0100

    Remove commented build module

--- build.zig
+++ build.zig
@@ -10,21 +10,4 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
-
-    // const exe = b.addExecutable(.{
-    //     .name = "sfl-template",
-    //     .root_module = b.createModule(.{
-    //         .root_source_file = b.path("src/main.zig"),
-    //         .optimize = .ReleaseSmall,
-    //         .target = b.resolveTargetQuery(.{
-    //             .cpu_arch = .wasm32,
-    //             .os_tag   = .freestanding,
-    //         })
-    //     })
-    // });
-    //
-    // exe.rdynamic = true;
-    // exe.entry    = .disabled;
-    //
-    // b.installArtifact(exe);
 }
